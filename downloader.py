dc10166e7ae2a785d06bb2a32a31034eb9295f15
carlosejimenez <cjsaltlake@gmail.com>
2019-02-12 02:20:10 -0700

    Change downloader.py to take command line argument and automatically download dataframes for given day. Also archiving example query for all indices.

--- downloader.py
+++ downloader.py
@@ -1,19 +1,54 @@
-import os
+import sys
+import re
+import datetime
 import requests
-import json
+import pandas
 
 
 if __name__ == '__main__':
-    headers = {
-        'X-DBP-APIKEY': 'e6e8d13f-2e66-476d-b375-c55b33eb7f8a',
-    }
 
-    params = (
-        ('date', '2018-06-28'),
-    )
+    # provide date to download all data for each company by command line argument
+    date = sys.argv[1]
 
-    url = 'https://api.developer.deutsche-boerse.com/prod/xetra-public-data-set/1.0.0/xetra'
+    dax = {'WDI': 'DE0007472060', 'DPW': 'DE0005552004', 'DBK': 'DE0005140008', 'RWE': 'DE0007037129',
+           'VNA': 'DE000A1ML7J1', 'LHA': 'DE0008232125', 'DB1': 'DE0005810055', 'TKA': 'DE0007500001',
+           'EOAN': 'DE000ENAG999', 'BAS': 'DE000BASF111', 'MUV2': 'DE0008430026', 'IFX': 'DE0006231004',
+           'BEI': 'DE0005200000', '1COV': 'DE0006062144', 'CON': 'DE0005439004', 'SAP': 'DE0007164600',
+           'HEI': 'DE0006047004', 'SIE': 'DE0007236101', 'HEN3': 'DE0006048432', 'ALV': 'DE0008404005',
+           'VOW3': 'DE0007664039', 'BAYN': 'DE000BAY0017', 'ADS': 'DE000A1EWWW0', 'FRE': 'DE0005785604',
+           'DAI': 'DE0007100000', 'FME': 'DE0005785802', 'DTE': 'DE0005557508', 'BMW': 'DE0005190003',
+           'MRK': 'DE0006599905', 'LIN': 'IE00BZ12WP82'}
 
-    response = requests.get(url, headers=headers, params=params)
 
-    response = response.json()
+    dax_copy = {}
+
+    for key in dax:
+        columns = ['Mnemonic', 'Date', 'Time', 'StartPrice', 'MaxPrice', 'MinPrice', 'EndPrice', 'TradedVolume', 'NumberOfTrades']
+
+        headers = {
+            'X-DBP-APIKEY': 'e6e8d13f-2e66-476d-b375-c55b33eb7f8a',
+        }
+
+        params = (
+            ('date', f'{date}'), ('limit', 1000), ('isin', f'{dax[key]}')
+        )
+
+        url = 'https://api.developer.deutsche-boerse.com/prod/xetra-public-data-set/1.0.0/xetra'
+
+        try:
+            response = requests.get(url, headers=headers, params=params)
+
+            response = response.json()
+            response_trimmed = []
+
+            for p in response:
+                entry = {i: p[i] for i in columns}
+                response_trimmed.append(entry)
+
+            df = pandas.DataFrame(response_trimmed, columns=columns)
+
+            df.to_feather(f'{key}-{date}.feather')
+
+        except:
+            print(f'{key} failed to write.')
+
